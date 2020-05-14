c7fdbc4c1b5683585b6a3ba06d077345bb499f20
hayakawa <so0sya9ge13@gmail.com>
2020-05-14 20:14:17 +0900

    no message

---
+++ WebApp/WebApp/Controllers/DiaryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class DiaryController : Controller
+    {
+			// ~/Diary/{*date}
+			public ActionResult Details(DateTime date) {
+				Models.Class1 class1 = new Models.Class1();
+				class1.Comment = "の日記";
+				class1.Date = DateTime.Now;
+				return View(class1);
+			}
+
+			// ~/Comment/{*date}
+			public ActionResult Comment(DateTime date)
+			{
+				
+
+				return View();
+			}
+		
+	}
+}
---
+++ WebApp/WebApp/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class Class1
+    {
+        public DateTime Date; 
+        public string Comment;
+    }
+}
